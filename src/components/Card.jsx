1a51203004f6f3c2c82f7028afe18573da8b557c
CrixDev <devoracristian100@gmail.com>
2024-12-30 19:27:45 -0700

    Seccion Que es Refactor, Graficos y componentes Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,21 @@
 
 
-function Card(){
+function Card({imagen, nombre, genero}){
 
-    const imagen = ["/assets/Undertale.png"]
-    
     return(
     
     
     
-        <div class="w-full max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
-        <img src={imagen} alt="" />
+        <div class="w-full max-w-xs overflow-hidden bg-black rounded-lg border-solid border-2 border-white shadow-lg ">
+        <img className="h-48 w-auto border-solid border-b-2 border-white" src={imagen} alt="" />
         <div class="py-5 text-center">
-            <a href="#" class="block text-xl font-bold text-gray-800 dark:text-white" tabindex="0" role="link">Undetale</a>
-            <span class="text-sm text-gray-700 dark:text-gray-200">RPG - Indie</span>
+            
+            <a href="#" class="block text-xl font-bold text-gray-800 dark:text-white" tabindex="0" role="link">{nombre}</a>
+            
+            <span class="text-sm text-gray-200">{genero}</span>
         </div>
     </div>
+    
     
     )
     }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,8 @@
 import Card from "./Card"
+
+
+import undertale from "/assets/Undertale.png"
+
 function Landing(){
 
     return(
@@ -14,7 +18,6 @@ function Landing(){
         <div  className=" w-full h-0.5 bg-white my-8"></div>
       
         <div className="overflow-hidden w-full h-28 ">
-        <img src="/assets/129471.gif" class="walking-gif gif1" alt="gif 1" />
         <img src="/assets/Mario.gif" class="walking-gif gif2" alt="gif 2" />
         <img src="/assets/Frisk.webp" class="walking-gif1 gif2" alt="gif 3" />
         </div>
@@ -22,19 +25,37 @@ function Landing(){
         <div className=" w-full h-0.5 bg-white my-8"></div>
           
         <div className="max-w-7xl py-10 mx-auto flex justify-between items-center">
-            <h1 className="text-white text-6xl font-rubik80s">Games Analyzed </h1> 
+            <h1 className="text-white text-6xl font-rubik80s">GAMES REFACTORED </h1> 
            
             
         </div>
             
-        <div className="max-w-7xl py-10 mx-auto flex justify-between items-center">
+        <div className="mx-28 flex justify-evenly items-center">
         
-        <Card />
+        <Card imagen={"/assets/Undertale.png"} nombre={"Undertale"} genero={"RPG - Indie"}  />
          
+        <Card imagen={"/assets/Celeste.png"} nombre={"Celeste"} genero={"Plataform - Indie"}  />
+         
+        <Card imagen={"/assets/Brawlhalla.jpg"} nombre={"Brawlhalla"} genero={"Fighting - Competitive"}  />
+         
+
         </div>
       
         <div className=" w-full h-0.5 bg-white my-8"></div>
         
+     
+        <div className="flex max-w-7l py-10 justify-evenly ">
+        <div className=" max-w-xl">
+        <h1 className="text-white text-4xl font-rubik80s">Que es Refactor the Game </h1> 
+        <h3 className="text-white text-xl font-rubik80s">Refactor The Game es un proyecto personal donde analizo juegos que me gustan y apartir de su arte, diseño y estilo adapto su concepto al desarrollo Web.</h3> 
+        
+        </div>
+          <div className="flex justify-center items-center bg-white h-96 w-96 rounded-3xl">
+           <img className="w-96" src="/assets/CrixPC.png" alt="" />
+           </div>
+        </div>
+                
+
     </div>
     )
 
